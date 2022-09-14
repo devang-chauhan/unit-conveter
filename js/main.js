056e3b227afda1e6e8c9fda1377b826fa15d9351
Devang <devngc@gmail.com>
2022-09-14 08:10:03 -0400

    chore(*): clean up html and JS

--- js/main.js
+++ js/main.js
@@ -1,54 +1,8 @@
-const numberEl = document.getElementById("number");
-const lengthEl = document.getElementById("length");
-const volumeEl = document.getElementById("volume");
-const massEl = document.getElementById("mass");
+const inputEl = document.getElementById("number");
+const convertEl = document.getElementById("convert");
 
-function update(number = 20) {
-  lengthEl.innerText =
-    number +
-    " meters = " +
-    Math.round(number * 3.28084, 3) +
-    " feet | " +
-    number +
-    " feet =" +
-    Math.round(number * 0.3048, 3) +
-    " meters";
 
-  volumeEl.innerText =
-    number +
-    " litres = " +
-    Math.round(number * 0.26417, 3) +
-    " gallons | " +
-    number +
-    " gallons =" +
-    Math.round(number * 3.78541, 3) +
-    " litres";
+convertEl.addEventListener("click", function () {
+  console.log(inputEl.value);
+});
 
-  massEl.innerText =
-    number +
-    " kilos = " +
-    Math.round(number * 2.20462, 3) +
-    " pounds | " +
-    number +
-    " pounds =" +
-    Math.round(number * 0.453592, 3) +
-    " kilos";
-}
-
-let number = 20;
-update();
-
-function convert() {
-  if (numberEl.value === "") {
-    number = 20;
-  } else {
-    number = numberEl.value;
-  }
-  update(number);
-}
-
-function reset() {
-  numberEl.value = "";
-  number = 20;
-  update();
-}
