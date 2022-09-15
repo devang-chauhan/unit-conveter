44e111b680cbe16dce1ac89620640d08045aa824
Devang <devngc@gmail.com>
2022-09-14 21:50:26 -0400

    fix(*): finish app

--- js/main.js
+++ js/main.js
@@ -4,21 +4,25 @@ const lengthEl = document.getElementById("length");
 const volumeEl = document.getElementById("volume");
 const massEl = document.getElementById("mass");
 
-function reset() {
-  lengthEl.innerText = ``;
-  volumeEl.innerText = ``;
-  massEl.innerText = ``;
-}
 
-convertEl.addEventListener("click", function () {
-  const val = parseFloat(inputEl.value);
+
+
+function convert(val) {
   if (isNaN(val)) {
     alert("Provide a valid number first.");
-    reset();
+    lengthEl.innerText = ``;
+    volumeEl.innerText = ``;
+    massEl.innerText = ``;
   } else {
     lengthEl.innerText = `${(val*3.281).toFixed(2)}`;
     volumeEl.innerText = `${(val * 0.264).toFixed(2)}`;
     massEl.innerText = `${(val*2.204).toFixed(2)}`;
   }
-});
+}
+convertEl.addEventListener("click", function () {
+  let input = parseFloat(inputEl.value);
+  convert(input);
+} );
 
+
+convert(parseFloat(inputEl.value));
